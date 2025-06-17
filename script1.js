14271c2660ccf9c994556782cb8ff88aa79a15b3
Carol-88 <gralleta@gmail.com>
2025-06-17 12:25:40 +0200

    ejercicio 1

---
+++ script1.js
@@ -0,0 +1,5 @@
+let firstNumber = prompt("Enter the first number:");
+let secondNumber = prompt("Enter the second number:");
+
+let res = firstNumber - secondNumber;
+console.log("The result of the subtraction is: " + res);
