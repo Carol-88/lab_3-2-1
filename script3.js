0400653ffbfb76380c2cb60aebbe864281390147
Carol-88 <gralleta@gmail.com>
2025-06-17 12:34:55 +0200

    ejercicio 3

---
+++ script3.js
@@ -0,0 +1,4 @@
+let radius = prompt("Enter the radius of the circle:");
+const pi = Math.PI;
+let area = pi * radius * radius;
+console.log("The area of the circle with radio" + radius + "is: " + area.toFixed(4));
