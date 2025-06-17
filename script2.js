52a33ad6d117cd8960eff51653f2ff2f7b2450c5
Carol-88 <gralleta@gmail.com>
2025-06-17 12:28:34 +0200

    ejercicio 2

---
+++ script2.js
@@ -0,0 +1,4 @@
+let name = prompt("Enter your name:");
+let city = prompt("Enter your city:");
+let hobby = prompt("Enter your hobby:");
+console.log("Your name is " + name + "! You live in " + city + " and your hobby is " + hobby + ".");
